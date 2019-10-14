a7f004291c41a52226553f7de78f209139d4b447
tomjmcnamee <tom@tomjmcnamee.com>
2019-10-14 09:30:54 -0400

    last before release

--- src/components/Person.js
+++ src/components/Person.js
@@ -7,7 +7,7 @@ const Person = (props) => {
         <div className="personDiv d-flex justify-content-between">
             <h1>{props.person.name}</h1>
             <div className="d-flex justify-content-start"><h3>Sneeze Count: {props.person.Sneezes}</h3></div>
-            <Button id="SneezeButton" onClick={() => {props.increaseSneezeCount(props.person)}}>Achoo!</Button>
+            <Button id="SneezeButton" className="btn btn-success" onClick={() => {props.increaseSneezeCount(props.person)}}>Achoo!</Button>
         </div>
     )   
 }
